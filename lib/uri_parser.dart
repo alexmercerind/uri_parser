15674a6ad8c787629d07c12a151a3b34f474ea1e
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-11-15 01:40:02 +0530

    initial commit

---
+++ lib/uri_parser.dart
@@ -0,0 +1,156 @@
+/// This file is a part of media_engine (https://github.com/alexmercerind/media_engine).
+/// Copyright © 2022 & onwards, Hitesh Kumar Saini <saini123hitesh@gmail.com>.
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU Affero General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU Affero General Public License for more details.
+///
+/// You should have received a copy of the GNU Affero General Public License
+/// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import 'dart:io';
+import 'package:safe_local_storage/safe_local_storage.dart';
+
+/// Type of URI interpreted by [URIParser].
+enum URIType {
+  /// A local [File] on the device.
+  file,
+
+  /// A local [Directory] on the device.
+  directory,
+
+  /// A remote [Uri] on the internet.
+  network,
+
+  /// A [Uri] that is not supported by [URIParser].
+  other,
+}
+
+/// URIParser
+/// ---------
+///
+/// Converts a [String] URI to usable [File], [Directory] or [Uri] result.
+///
+class URIParser {
+  /// The [String] URI to be parsed.
+  final String? data;
+
+  /// Whether to log errors to the console or not.
+  final bool verbose;
+
+  /// Type of the URI.
+  URIType type = URIType.other;
+
+  /// [File] interpreted from the URI.
+  File? file;
+
+  /// [Directory] interpreted from the URI.
+  Directory? directory;
+
+  /// [Uri] interpreted from the URI.
+  Uri? uri;
+
+  /// URIParser
+  /// ---------
+  ///
+  /// Converts a [String] URI to usable [File], [Directory] or [Uri] result.
+  ///
+  URIParser(
+    this.data, {
+    this.verbose = true,
+  }) {
+    var value = data?.trim();
+    if (value != null) {
+      // Get rid of quotes, if any.
+      if (value.startsWith('"') && value.endsWith('"')) {
+        value = value.substring(1, value.length - 1);
+      }
+      // Make sure to convert file:// URI with two slashes to three slashes.
+      // Since [Uri.parse] doesn't support file:// URI with two slashes.
+      if (value.toLowerCase().startsWith('file://') &&
+          !value.toLowerCase().startsWith('file:///')) {
+        value = 'file:///${value.substring(7)}';
+      }
+
+      debugPrint(value.toString());
+      // Resolve the file:// scheme.
+      try {
+        final resource = Uri.parse(value);
+        if (resource.isScheme('FILE')) {
+          var path = resource.toFilePath();
+          // Replace forward slashes with backslashes on Windows, to make [FS] work correctly.
+          if (Platform.isWindows) {
+            path = path.replaceAll('/', '\\');
+          }
+          if (FS.typeSync_(path) == FileSystemEntityType.file) {
+            if (Platform.isWindows) {
+              path = path.replaceAll('\\', '/');
+            }
+            type = URIType.file;
+            file = File(path);
+          }
+          if (FS.typeSync_(path) == FileSystemEntityType.directory) {
+            if (Platform.isWindows) {
+              path = path.replaceAll('\\', '/');
+            }
+            type = URIType.directory;
+            directory = Directory(path);
+          }
+        }
+        // Resolve the network scheme.
+        else if (resource.isScheme('HTTP') ||
+            resource.isScheme('HTTPS') ||
+            resource.isScheme('FTP') ||
+            resource.isScheme('RTSP') ||
+            resource.isScheme('RTMP')) {
+          type = URIType.network;
+          uri = resource;
+        }
+      } catch (exception, stacktrace) {
+        // Likely [FormatException] from [Uri.parse].
+        debugPrint(exception.toString());
+        debugPrint(stacktrace.toString());
+      }
+      // Resolve direct [File] or [Directory] paths.
+      if (type == URIType.other) {
+        try {
+          // Replace forward slashes with backslashes on Windows, to make [FS] work correctly.
+          if (Platform.isWindows) {
+            value = value.replaceAll('/', '\\');
+          }
+          if (FS.typeSync_(value) == FileSystemEntityType.file) {
+            if (Platform.isWindows) {
+              value = value.replaceAll('\\', '/');
+            }
+            type = URIType.file;
+            file = File(value);
+          }
+          if (FS.typeSync_(value) == FileSystemEntityType.directory) {
+            if (Platform.isWindows) {
+              value = value.replaceAll('\\', '/');
+            }
+            type = URIType.directory;
+            directory = Directory(value);
+          }
+        } catch (exception, stacktrace) {
+          // Likely [FormatException] from [Uri.parse].
+          debugPrint(exception.toString());
+          debugPrint(stacktrace.toString());
+        }
+      }
+    }
+  }
+
+  /// Prints the passed [object] to the console.
+  void debugPrint(String message) {
+    if (verbose) {
+      print(message);
+    }
+  }
+}
---
+++ test/uri_parser_test.dart
@@ -0,0 +1,93 @@
+import 'package:test/test.dart';
+import 'package:uri_parser/uri_parser.dart';
+
+void main() {
+  test('file:// File', () {
+    final parser = URIParser('file://C:/Windows/explorer.exe');
+    expect(parser.type, URIType.file);
+    expect(parser.file, isNotNull);
+    expect(parser.directory, isNull);
+    expect(parser.uri, isNull);
+  });
+  test('file:// Directory', () {
+    final parser = URIParser('file://C:/Windows/System32');
+    expect(parser.type, URIType.directory);
+    expect(parser.file, isNull);
+    expect(parser.directory, isNotNull);
+    expect(parser.uri, isNull);
+  });
+  test('file:// File with backward slashes', () {
+    final parser = URIParser('file://C:\\Windows\\explorer.exe');
+    expect(parser.type, URIType.file);
+    expect(parser.file, isNotNull);
+    expect(parser.directory, isNull);
+    expect(parser.uri, isNull);
+  });
+  test('file:// Directory with backward slashes', () {
+    final parser = URIParser('file://C:\\Windows\\System32');
+    expect(parser.type, URIType.directory);
+    expect(parser.file, isNull);
+    expect(parser.directory, isNotNull);
+    expect(parser.uri, isNull);
+  });
+  test('raw path File', () {
+    final parser = URIParser('C:/Windows/explorer.exe');
+    expect(parser.type, URIType.file);
+    expect(parser.file, isNotNull);
+    expect(parser.directory, isNull);
+    expect(parser.uri, isNull);
+  });
+  test('raw path Directory', () {
+    final parser = URIParser('C:/Windows/System32');
+    expect(parser.type, URIType.directory);
+    expect(parser.file, isNull);
+    expect(parser.directory, isNotNull);
+    expect(parser.uri, isNull);
+  });
+  test('raw path File with backward slashes', () {
+    final parser = URIParser('C:\\Windows\\explorer.exe');
+    expect(parser.type, URIType.file);
+    expect(parser.file, isNotNull);
+    expect(parser.directory, isNull);
+    expect(parser.uri, isNull);
+  });
+  test('raw path Directory with backward slashes', () {
+    final parser = URIParser('C:\\Windows\\System32');
+    expect(parser.type, URIType.directory);
+    expect(parser.file, isNull);
+    expect(parser.directory, isNotNull);
+    expect(parser.uri, isNull);
+  });
+  test('network URIs', () {
+    final http = URIParser('http://www.test.com');
+    expect(http.type, URIType.network);
+    expect(http.file, isNull);
+    expect(http.directory, isNull);
+    expect(http.uri, isNotNull);
+    final https = URIParser('https://www.test.com');
+    expect(https.type, URIType.network);
+    expect(https.file, isNull);
+    expect(https.directory, isNull);
+    expect(https.uri, isNotNull);
+    final ftp = URIParser('ftp://www.test.com');
+    expect(ftp.type, URIType.network);
+    expect(ftp.file, isNull);
+    expect(ftp.directory, isNull);
+    expect(ftp.uri, isNotNull);
+    final rtsp = URIParser('rtsp://www.test.com');
+    expect(rtsp.type, URIType.network);
+    expect(rtsp.file, isNull);
+    expect(rtsp.directory, isNull);
+    expect(rtsp.uri, isNotNull);
+    final rtmp = URIParser('rtmp://www.test.com');
+    expect(rtmp.type, URIType.network);
+    expect(rtmp.file, isNull);
+    expect(rtmp.directory, isNull);
+    expect(rtmp.uri, isNotNull);
+  });
+  test('unknown URIs', () {
+    final parser = URIParser('unknown://www.test.com');
+    expect(parser.type, URIType.other);
+    expect(parser.uri, isNull);
+  });
+}
